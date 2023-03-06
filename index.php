b1e938002568f3d2a4d883d985c5660f1bb17eaa
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-06 10:42:20 -0500

    Data Types - Strings, Integers, Floats, booleans

--- index.php
+++ index.php
@@ -12,3 +12,36 @@
 
 <h1>Variables</h1>
 
+<?php
+	
+	/********** Variables *********
+	 * Prefix $ (ugh)
+	 * Can only start with a letter or underscore
+	 * Only letters, numbers or underscores in var name
+	 */
+
+	#Data Types
+		/* 
+		String
+		Integers
+		floats
+		booleans
+		Arrays
+		Objects
+		NULL
+		Resource
+		*/
+	$_num1 = 1;
+	$_num2 = 4;
+	$sum = $_num1 + $_num2;
+
+	$string1 = 'Hello';
+	$string2 = 'World';
+	$concat = $string1 . ' ' . $string2; // Concatenation
+
+	$float1 = 4.4;
+	$bool1 = true;
+
+	echo $sum;
+?>
+
