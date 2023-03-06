4953f9b65ee76f6ac10a8e8fd92afde8e752dbcc
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-06 16:17:03 -0500

    Associative arrays overview

--- arrays.php
+++ arrays.php
@@ -20,9 +20,20 @@
 	$ids[] = 6; // Adds to last position
 
 	//Some important array functions
-	count($ids); //Counts number of items in array
-	print_r($ids); //echo for arrays basically
-	var_dump($ids); //print_r with more data like data type
+		// count($ids); //Counts number of items in array
+		// print_r($ids); //echo for arrays basically
+		// var_dump($ids); //print_r with more data like data type
+
+	//Associative arrays (Define keys and value)
+	$people2 = array('Matt' => 31, 'Erin' => 34, 'Glenn' => 62, 'Michele' => 59);
+	echo $people2['Matt']; //31
+	$ids2 = [31 => 'Matt', 34 => 'Erin', 62 => 'Glenn', 59 => 'Michele'];
+	echo $ids2[31];
+
+	$people2['Jill' => 45]; //Add the same way as indexed arrays basically
+		count($ids2);
+		print_r($ids2);
+		var_dump($ids2);
 
 	
 
