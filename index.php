7f1b2059828796329c23cc5faac770f6da4b6d6c
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-06 16:04:26 -0500

    Basic overview of data types, concatenation, constants, bools

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
 
 <?php
 	
-	/********** Variables *********
+	/********** Variables
 	 * Prefix $ (ugh)
 	 * Can only start with a letter or underscore
 	 * Only letters, numbers or underscores in var name
 	 */
 
-	#Data Types
-		/* 
+	// Data Types
+	/* 
 		String
 		Integers
 		floats
@@ -30,7 +30,8 @@
 		Objects
 		NULL
 		Resource
-		*/
+	*/
+
 	$_num1 = 1;
 	$_num2 = 4;
 	$sum = $_num1 + $_num2;
@@ -38,10 +39,17 @@
 	$string1 = 'Hello';
 	$string2 = 'World';
 	$concat = $string1 . ' ' . $string2; // Concatenation
+	$greeting2 = "$string1 $string2"; //will show correctly, single quotes won't
+
+	$string3 = "They're here"; //Single quote in string (or can use a backslash like 'They\'re here")
 
 	$float1 = 4.4;
 	$bool1 = true;
 
-	echo $sum;
+	//Constants 
+	define('GREETING', 'Constant'); // Constants are case sensitive by default - can change by adding  (... , ... , true) to end;
+
+	echo GREETING;
+	
 ?>
 
