07589cd263ca5f5bd60144e7cb65f071bb30a92d
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-08 20:52:24 -0500

    equals and not equal conditions

---
+++ conditions.php
@@ -0,0 +1,38 @@
+<?php
+
+	// CONDITIONALS
+
+	/*
+		==
+		===
+
+		< 
+		>
+		<=
+		>=
+		!=
+		!==
+	*/
+
+	$num = 5;
+
+	if($num === 5) {
+		echo "5 passed";
+	} else {
+		echo "Does not equal";
+	}
+
+	$stringNum = "5";
+	if($num == 5) {
+		echo "5 passed";
+	} else {
+		echo "5 did not pass";
+	}
+
+	$number = 6;
+
+	if($number != 5) {
+		echo "5 passed";
+	} else {
+		echo "Did not pass";
+	}
