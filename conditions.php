0f9b9024a7f4a0031ae9ec2dcc2be6852ecae8a6
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-10 11:56:07 -0500

    If statements, operators and Switch

--- conditions.php
+++ conditions.php
@@ -17,14 +17,14 @@
 	$num = 5;
 
 	if($num === 5) {
-		echo "5 passed";
+		echo "5 passed<br>";
 	} else {
 		echo "Does not equal";
 	}
 
 	$stringNum = "5";
 	if($num == 5) {
-		echo "5 passed";
+		echo "5 passed<br>";
 	} else {
 		echo "5 did not pass";
 	}
@@ -32,7 +32,55 @@
 	$number = 6;
 
 	if($number != 5) {
-		echo "5 passed";
+		echo "5 passed<br>";
 	} else {
 		echo "Did not pass";
+	}
+
+	$num2 = 5;
+
+	if($num2 > 4) {
+		if($num2 < 10) {
+			echo "$num2 passed!<br>";
+		}
+	}
+
+	/* Logical Operators
+
+	and && - both are true
+	or ||	- either or is true, or one of them is true
+	xor - ONE is true, but not both
+
+	*/
+
+	if($num2 > 4 && $num2 < 10) {
+		echo "$num2 passed!<br>";
+	}
+
+	if($num2 > 12 OR $num2 < 4) {
+		echo "$num2 OR passed<br>";
+	}
+
+	if ($num2 > 15 XOR $num2 === 1 ) {
+		echo "$num2 XOR passed!";
+	}
+	echo "<br><br>";
+	//Switch
+	$favColor = 'red';
+
+	switch($favColor) {
+		case 'red': 
+			echo 'Your favourite color is red';
+		break;
+		case 'green': 
+			echo 'Your favourite color is green';
+		break;
+		case 'blue': 
+			echo 'Your favourite color is blue';
+		break;
+		case 'yellow': 
+			echo 'Your favourite color is yellow';
+		break;
+		default: 
+			echo 'Your favourite color is something else';
 	}
