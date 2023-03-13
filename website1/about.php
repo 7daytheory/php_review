4da402d5e521d91bcf2850e8ea479c4236050aca
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-13 12:59:15 -0400

    Setting up super basic website pages for example with includes/require

---
+++ website1/about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website1 - Example</title>
+</head>
+<body>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+    <h1>About</h1>
+
+    <footer>
+        <p>MyWebsite &copy; 2023</p>
+    </footer>
+    
+</body>
+</html>
