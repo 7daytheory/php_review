a1247d8e07b46652b3e1a12d16ea1f9ac3d97db3
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-16 11:26:47 -0400

    Merging arrays

--- array_functions.php
+++ array_functions.php
@@ -28,3 +28,36 @@ array_shift($fruits); // Removes item from start of array
 
 echo "<br><h3>Array List</h3>";
  print_r($fruits);
+
+ echo "<br><br><h2>Combining/Merging Arrays</h2>";
+
+ //Merging arrays
+ $arr1 = [1,2,3];
+ $arr2 = [4,5,6];
+
+ $arr3 = array_merge($arr1, $arr2); // Merges arr1 and arr2
+
+ $arr_3 = [...$arr1, ...$arr2]; // Using spread operator to merge
+ 
+ print_r($arr3);
+ print_r($arr_3);
+
+ echo "<br><br><h3>Use array as keys when merging another array</h3>";
+
+ $a = ["Green", "Red", "Blue"];
+ $b = ["Unripe Banana", "Apple", "Blueberry"];
+
+ $c = array_combine($a, $b); // Merges array, uses $a as keys to $b
+ print_r($c);
+
+ $keys = array_keys($c);
+ print_r($keys); // Show all the keys in the array ($a values in this isntance)
+
+ $flipped = array_flip($c); //Flips keys/values ($b keys to $a);
+ print_r($flipped);
+
+ $numbers = range(1,20); //Creates array to range from 1 -> 20 (1,2,3 ... 18, 19, 20)
+ print_r($numbers);
+
+
+ 
