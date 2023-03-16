04ef6f6f00409c2211020bfc7e1f7d6fbf383781
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-16 12:43:11 -0400

    Cleaner code to check for GET or POST values

--- get_post.php
+++ get_post.php
@@ -5,26 +5,16 @@
     /* Getting data through forms using these superglobals */
     
     echo "<h2>GET Response</h2><br>";
-
-    if(isset($_GET)){
+    if(!empty($_GET)){
         echo "Age : " . $_GET['age'] . "<br>";
         echo "Name : " . $_GET['name'] . "<br>";
-        } else {
-            echo "No Age variable found in URL<br><br>";
-        }
+     }
 
-        echo "<h2>POST Response</h2><br>";
-        if(isset($_POST['name'])) {
-            echo "Name : " . $_POST['name'] . "<br>";
-            } else {
-                echo "No Name variable found in URL<br>";
-            }
-        
-            if(isset($_POST['age'])) {
-                echo "Age : " . $_POST['age'] . "<br>";
-                } else {
-                    echo "No Age variable found in URL<br><br>";
-                }
+    echo "<h2>POST Response</h2><br>";
+    if(isset($_POST['submit'])) {
+        echo "Name : " . $_POST['name'] . "<br>";
+        echo "Age : " . $_POST['age'] . "<br>";
+    }
 ?>
 
 <a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Matt&age=31">Click</a>
