1f7d1ed2117756741f4e1597c12f88b1918d9319
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-16 12:21:59 -0400

    Overview of each superglobal and what they are used for

---
+++ website1/superglobals.php
@@ -0,0 +1,23 @@
+<?php 
+
+/******************* Superglobal **************/
+
+/* Overview of list of superglobals 
+
+$_GET - Contains informations about the variables passed through the URL
+
+$_POST - Contains information about variables passed through a form(generally)
+
+$_COOKIES - Contains info about variables passed through a cookie 
+
+$_SESSION - contains information about variables passed through a session 
+
+$_SERVER - Contains information about the server environment
+
+$_ENV - Contains informations about the environment variables 
+
+$_FILES - Contains information about files uploaded to the script 
+
+$_REQUEST - Contains Informations about variables passed through the form or URL 
+
+*/
