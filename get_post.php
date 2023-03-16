5bdacc0f143932b72aeb7c20f90219c6e444ed67
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-16 12:29:40 -0400

    Added isset check for GET superglobals

--- get_post.php
+++ get_post.php
@@ -3,9 +3,18 @@
     /* ---- $_GET and $_POST superglobal --- */
 
     /* Getting data through forms using these superglobals */
+    
+    if(isset($_GET['name'])) {
+    echo "Name : " . $_GET['name'] . "<br>";
+    } else {
+        echo "No Name variable found in URL<br>";
+    }
 
-    echo $_GET['name'];
-    echo $_GET['age'];
+    if(isset($_GET['age'])) {
+        echo "Age : " . $_GET['age'] . "<br>";
+        } else {
+            echo "No Age variable found in URL<br>";
+        }
 ?>
 
 <a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Matt&age=31">Click</a>
