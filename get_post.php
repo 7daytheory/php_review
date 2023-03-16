5fa052b3159f33198d886647d75a313ac6dd9be0
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-03-16 12:39:27 -0400

    Added form with POST to submit form and get values

--- get_post.php
+++ get_post.php
@@ -4,28 +4,40 @@
 
     /* Getting data through forms using these superglobals */
     
-    if(isset($_GET['name'])) {
-    echo "Name : " . $_GET['name'] . "<br>";
-    } else {
-        echo "No Name variable found in URL<br>";
-    }
+    echo "<h2>GET Response</h2><br>";
 
-    if(isset($_GET['age'])) {
+    if(isset($_GET)){
         echo "Age : " . $_GET['age'] . "<br>";
+        echo "Name : " . $_GET['name'] . "<br>";
         } else {
-            echo "No Age variable found in URL<br>";
+            echo "No Age variable found in URL<br><br>";
         }
+
+        echo "<h2>POST Response</h2><br>";
+        if(isset($_POST['name'])) {
+            echo "Name : " . $_POST['name'] . "<br>";
+            } else {
+                echo "No Name variable found in URL<br>";
+            }
+        
+            if(isset($_POST['age'])) {
+                echo "Age : " . $_POST['age'] . "<br>";
+                } else {
+                    echo "No Age variable found in URL<br><br>";
+                }
 ?>
 
 <a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Matt&age=31">Click</a>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<?php echo "<br><br><h3>Form</h3>"; ?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         <div>
             <label for="name">Name</label>
             <input type="text" name="name">
         </div>
         <div>
-            <label for="age">Name</label>
+            <label for="age">Age</label>
             <input type="text" name="age">
         </div>
         <div>
