04fccbb3acc3b15a32af66e9c7f1c7a0807ab33f
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-12 14:37:04 -0400

    Commented out past overviews, labelled each one

--- OOP/index.php
+++ OOP/index.php
@@ -13,6 +13,7 @@
     <body>
 
         <?php
+            /******* Basic setting up class instances with objects, calling class and displaying values *******/
             //include("includes/person.inc.php");
             // $pet01 = new Pet();
 
@@ -33,18 +34,26 @@
             // $person02->setName("Daniel");
             // echo $person02->name;
 
-            $person1 = new Person("Matthew", 32, "Blonde", "5'11"); //Pass in properties to Class
+            /******* Setting prop values when setting up instance of class *******/
+
+            //$person1 = new Person("Matthew", 32, "Blonde", "5'11"); //Pass in properties to Class
             // echo "Name: " . $person1->name . " Age :" . $person1->age . " Hair: " . $person1->hair . " Height: " . $person1->height . " Company : " . $person1->company;
 
             // $person1->setName("John");
             // echo "<br>" . $person1->name;
+            // echo $person1->getName();
+
+
+            // /***** Deleting an object ******/
+
+            // $object = new NewClass();
+            // unset($object); //delete/unset object using built in method
+            // echo $object->getProperty(); // Can't get values bc it was unset above
+
+            /******** Static methods and properties ********/
+
+            $person1 = new Person("Matthew", 32, "Blonde", "5'11");
             echo $person1->getName();
-
-
-            /***** Deleting an object ******/
-            $object = new NewClass();
-            unset($object); //delete/unset object using built in method
-            echo $object->getProperty(); // Can't get values bc it was unset above
 
         ?>
 
