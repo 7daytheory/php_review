3e583e07396c0956b5f93ad80583dc2fcdbf413e
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-12 14:09:36 -0400

    Use setName method to update name of object

--- OOP/index.php
+++ OOP/index.php
@@ -35,6 +35,9 @@
             $person1 = new Person("Matthew", 32, "Blonde", "5'11"); //Pass in properties to Class
             echo "Name: " . $person1->name . " Age :" . $person1->age . " Hair: " . $person1->hair . " Height: " . $person1->height . " Company : " . $person1->company;
 
+            $person1->setName("John");
+            echo "<br>" . $person1->name;
+
         ?>
 
     </body>
